b3e2a971f91cf657adcc9f3f2a77b83fd8dbef27
Ata Kocogullari <ata.kocogullari@sabanciuniv.edu>
2024-10-31 19:32:41 +0300

    Commit mesajı burada

--- server/routes/auth/auth-routes.js
+++ server/routes/auth/auth-routes.js
@@ -1,8 +1,11 @@
-const express = require('express')
-const {registerUser,loginUser, resetPassword} = require('../../controllers/auth/auth-controller')
+const express = require('express');
+const { registerUser, loginUser, resetPassword, confirmDeletion, deleteUserAccount } = require('../../controllers/auth/auth-controller');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/reset-password', resetPassword);
+router.post('/confirm-delete', confirmDeletion); 
+router.delete('/delete-account', deleteUserAccount);
+
 module.exports = router;
