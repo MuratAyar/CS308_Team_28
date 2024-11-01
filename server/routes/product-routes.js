adff9342318a1b8a319c3883a84a218dae593ef7
Yasin Mirioğlu <ysnmirioglu@gmail.com>
2024-11-01 16:24:53 +0300

    Add sorting functionality by price and popularity for products

--- server/routes/product-routes.js
+++ server/routes/product-routes.js
@@ -2,20 +2,6 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/Product');
 
-
-router.get('/all', async (req, res) => {
-    console.log("GET /all endpoint hit");
-    try {
-        const products = await Product.find({});
-        console.log("Products:", products); // Log products to confirm
-        res.json(products);
-    } catch (error) {
-        console.error('Error fetching all products:', error);
-        res.status(500).json({ error: 'Could not fetch products.' });
-    }
-});
-
-
 router.get('/search', async (req, res) => {
     let { query } = req.query;
 
@@ -41,6 +27,27 @@ router.get('/search', async (req, res) => {
         res.status(500).json({ error: 'Server error' });
     }
 });
+
+
+router.get('/all', async (req, res) => {
+    const { sort, order = 'asc' } = req.query;
+
+    let sortOptions = {};
+    if (sort === 'price') {
+        sortOptions.price = order === 'desc' ? -1 : 1;
+    } else if (sort === 'popularity') {
+        sortOptions.popularity = order === 'desc' ? -1 : 1;
+    }
+
+    try {
+        const products = await Product.find({}).sort(sortOptions);
+        res.json(products);
+    } catch (error) {
+        console.error('Error fetching and sorting products:', error);
+        res.status(500).json({ error: 'Could not fetch products.' });
+    }
+});
+
 
 
 module.exports = router;
